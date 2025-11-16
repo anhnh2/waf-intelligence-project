d584c547b20e6bc0ae4c2ad17cc5819d12c37450
Tomcuteee <139209153+Tomcuteee@users.noreply.github.com>
2025-11-16 13:53:53 +0700

    Update vulnerbale.php

--- vulnerbale.php
+++ vulnerbale.php
@@ -1,1 +1,10 @@
-aaa
+<?php
+// vulnerable.php - command injection demo (lab only)
+$cmd = isset($_GET['cmd']) ? $_GET['cmd'] : '';
+if($cmd) {
+    $output = shell_exec($cmd);
+    echo "<pre>$output</pre>";
+} else {
+    echo "no cmd";
+}
+?>
